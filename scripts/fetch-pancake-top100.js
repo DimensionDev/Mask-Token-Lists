fd15e8978e6b5fccf5d477553d5b0e1bd6c0b61d
Shiyong Chen <unclebill@mindhand.io>
2021-06-04 18:48:50 +0800

    :chore: sync token list from PancakeSwap with Github Action (#7)

---
+++ scripts/fetch-pancake-top100.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
+
+const url = "https://tokens.pancakeswap.finance/pancakeswap-top-100.json";
+const fileDest = path.resolve(__dirname, "../src/erc20/pancake-top100.json");
+
+async function fetchTop100() {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const tokens = data.tokens.map(({ name, symbol, address, decimals }) => ({
+    name,
+    symbol,
+    address,
+    decimals,
+  }));
+
+  console.log("tokens:");
+  console.log(tokens);
+
+  fs.writeFileSync(fileDest, JSON.stringify(tokens, null, 2));
+}
+
+fetchTop100();
