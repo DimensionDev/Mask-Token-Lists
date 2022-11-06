c669d38c35183a2ab9ff2cd6a9e206b3a624a6ae
Lantt <laanfor@gmail.com>
2022-11-07 00:01:01 +0800

    chore: add cache folder

--- scripts/providers/subScan.ts
+++ scripts/providers/subScan.ts
@@ -33,9 +33,26 @@ export class SubScan implements Provider {
   async getTopTokens() {
     let page: number | undefined = 0
     let result: TokenInfo[] = []
-    while (page) {
+    while (page !== undefined) {
       const url = urlcat(baseURL, '/api/scan/evm/tokens')
-      const list = await axios.post<TokenResult>(url, { page, row: 100, search: '' })
+      const list = await axios.post<TokenResult>(
+        url,
+        {
+          page,
+          row: 100,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            accept: 'application/json, text/plain, */*',
+            'content-type': 'application/json; charset=utf-8',
+            'access-control-allow-credentials': true,
+            'User-Agent':
+              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+          },
+        },
+      )
+      console.log(list.data)
       if (list.data.code === 0 && list.data.data.list.length) {
         result = [...result, ...list.data.data.list]
         page++
@@ -55,17 +72,20 @@ export class SubScan implements Provider {
 
   async generateFungibleTokens(chainId: ChainId, exclude: FungibleToken[]): Promise<FungibleToken[]> {
     const allTokens = await this.getTopTokens()
-    const topList = allTokens.map(
-      (x) =>
-        ({
-          // The subScan not provide the logo url
-          chainId,
-          address: toChecksumAddress(x.contract),
-          name: x.name,
-          symbol: x.symbol,
-          decimals: x.decimals,
-        } as FungibleToken),
-    )
+    const topList = allTokens
+      .filter((x) => x.contract && x.symbol)
+      .map(
+        (x) =>
+          ({
+            // The subScan not provide the logo url
+            chainId,
+            address: toChecksumAddress(x.contract),
+            name: x.name,
+            symbol: x.symbol,
+            decimals: x.decimals,
+          } as FungibleToken),
+      )
+
     return [...topList, ...exclude]
   }
 
