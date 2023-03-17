d2b2b381fc0ed2c68f8cb824afbc8934ce3c5b74
zhouhanseng <z308114274@gmail.com>
2023-03-17 09:36:36 +0800

    chore: remove useless code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Package from '../package.json'
 import { mkdir, writeFile } from 'fs/promises'
 import { resolve } from 'path'
 import { generate as generateFungibleTokens } from './generate-fungible-tokens'
-import { generate as generateNonFungibleTokens } from './generate-fungible-tokens'
 import { getEnumAsArray } from './helpers'
 import { ChainId } from './types'
 
@@ -18,14 +17,6 @@ async function main() {
     // vx.x.x/tokens.ts
     await mkdir(pathToVersionFolder, { recursive: true })
     await writeFile(`${pathToVersionFolder}/tokens.json`, await generateFungibleTokens(value.value))
-
-    // lastest/non-fungible-tokens.ts
-    await mkdir(pathToLatestFolder, { recursive: true })
-    await writeFile(`${pathToLatestFolder}/non-fungible-tokens.json`, await generateNonFungibleTokens(value.value))
-
-    // vx.x.x/non-fungible-tokens.ts
-    await mkdir(pathToVersionFolder, { recursive: true })
-    await writeFile(`${pathToVersionFolder}/non-fungible-tokens.json`, await generateNonFungibleTokens(value.value))
 
     console.log(`Genereated fungible token list for ${value.key} (Chain ID: ${value.value}).`)
   }
