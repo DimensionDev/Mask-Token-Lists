042d9012cb6ff296731729f7a60f9da2cfa97e74
zhouhanseng <z308114274@gmail.com>
2023-03-23 10:41:49 +0800

    chore: debug

--- scripts/providers/explorer.ts
+++ scripts/providers/explorer.ts
@@ -26,7 +26,8 @@ export class Explorer implements Provider {
       const pageData = await fetchExplorerPage(url)
       const q = cheerio.load(pageData)
       const table = q('#tblResult tbody tr').map((_, x) => x)
-      console.log({ pageData })
+      const table2 = q('table tbody tr').map((_, x) => x)
+      console.log({ pageData, table, table2 })
       // @ts-ignore
       for (const x of table) {
         const logo = q('img', x).attr('src')
