af5a30431388e2207fda37e77de0b0699cfe7723
zhouhanseng <z308114274@gmail.com>
2023-03-25 09:53:22 +0800

    feat: fetch fantom

--- scripts/utils/base.ts
+++ scripts/utils/base.ts
@@ -2,6 +2,7 @@ import { ChainId, FungibleToken } from '../type'
 import { fetchAurora } from './explorers/aurora'
 import { fetchOptimistic } from './explorers/optimistic'
 import { fetchGnosis } from './explorers/gnosis'
+import { fetchFantom } from './explorers/fantom'
 
 export function convertEnumToArray(e: any) {
   return Object.keys(e)
@@ -20,7 +21,7 @@ export const explorerPagesMapping: Partial<Record<ChainId, string[]>> = {
   [ChainId.Polygon]: [],
   [ChainId.Arbitrum]: [],
   [ChainId.Avalanche]: [],
-  [ChainId.Fantom]: [],
+  [ChainId.Fantom]: ['https://ftmscan.com/tokens?ps=100', 'https://ftmscan.com/tokens?p=2&ps=100'],
   [ChainId.xDai]: ['https://gnosisscan.io/tokens'],
   [ChainId.Aurora]: ['https://explorer.aurora.dev/tokens'],
   [ChainId.Optimistic]: ['https://optimistic.etherscan.io/tokens'],
@@ -32,7 +33,7 @@ export const explorerFetchMapping: Partial<Record<ChainId, (url: string) => Prom
   [ChainId.Polygon]: async (url: string) => [],
   [ChainId.Arbitrum]: async (url: string) => [],
   [ChainId.Avalanche]: async (url: string) => [],
-  [ChainId.Fantom]: async (url: string) => [],
+  [ChainId.Fantom]: fetchFantom,
   [ChainId.xDai]: fetchGnosis,
   [ChainId.Aurora]: fetchAurora,
   [ChainId.Optimistic]: fetchOptimistic,
---
+++ scripts/utils/explorers/fantom.ts
@@ -0,0 +1,38 @@
+import { ChainId, FungibleToken } from '../../type'
+import { toChecksumAddress } from 'web3-utils'
+import { createFungibleToken } from '../createFungibleToken'
+import * as cheerio from 'cheerio'
+import puppeteer from 'puppeteer'
+
+export async function fetchFantom(url: string) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  page.once('load', () => console.log('Fantom Page loaded!'))
+  page.once('error', (error) => console.log('Failed to Fantom Page load!', error))
+  await page.setViewport({ width: 1080, height: 1024 })
+
+  const tableSelector = '#ContentPlaceHolder1_divresult'
+  const tableElementHandler = await page.waitForSelector(tableSelector)
+  const tableElement = await tableElementHandler?.evaluate((x) => x.innerHTML)
+
+  await browser.close()
+  const q = cheerio.load(tableElement ?? '')
+  const table = q('table tbody tr').map((_, x) => x)
+  let results: FungibleToken[] = []
+
+  for (const x of table) {
+    const logo = q('img', x).attr('src')
+    const fullName = q('.media .media-body a', x).text()
+    if (!fullName) continue
+
+    const pageLink = q('.media .media-body a', x).attr('href')
+    if (!pageLink) continue
+
+    const address = toChecksumAddress(pageLink?.replace('/token/', ''))
+    if (!address) continue
+
+    results.push(createFungibleToken(ChainId.Fantom, address, fullName, 18, logo ? `https://ftmscan.com${logo}` : ''))
+  }
+  return results
+}
