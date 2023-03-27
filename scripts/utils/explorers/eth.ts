5301b80dd4f259990e0c7d0651b455d07e3f57cb
zhouhanseng <z308114274@gmail.com>
2023-03-27 11:56:46 +0800

    chore: all explorer works

--- scripts/utils/explorers/eth.ts
+++ scripts/utils/explorers/eth.ts
@@ -9,7 +9,6 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 puppeteer.use(StealthPlugin())
 
 export async function fetchETH(url: string) {
-  puppeteer.use(StealthPlugin())
   const browser = await puppeteer.launch({ executablePath: executablePath(), timeout: 1000000 })
   const page = await browser.newPage()
   await page.goto(url)
