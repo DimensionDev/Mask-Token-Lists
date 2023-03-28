fb9760e7874aec40bd46a07d75f0a70735750218
zhouhanseng <z308114274@gmail.com>
2023-03-28 10:21:06 +0800

    chore: write to dist

--- scripts/utils/file.ts
+++ scripts/utils/file.ts
@@ -14,11 +14,11 @@ export const pathToLatestFolder = path.join(__dirname, `dist/latest`)
 export const cryptoRankcacheDir = path.join(process.env.PWD, 'scripts/cache/cryptorank')
 
 export async function writeTokensToFile(chain: ChainId, tokens: FungibleToken[]) {
-  await mkdir(pathToVersionFolder, { recursive: true })
+  await mkdir(path.join(pathToVersionFolder, chain.toString()), { recursive: true })
   await fs.writeFile(path.join(pathToVersionFolder, chain.toString(), 'tokens.json'), generate(tokens), {
     encoding: 'utf-8',
   })
-  await mkdir(pathToLatestFolder, { recursive: true })
+  await mkdir(path.join(pathToLatestFolder, chain.toString()), { recursive: true })
   await fs.writeFile(path.join(pathToLatestFolder, chain.toString(), 'tokens.json'), generate(tokens), {
     encoding: 'utf-8',
   })
