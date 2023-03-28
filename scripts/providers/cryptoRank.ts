6d0f103852f65ad57c6b8409cce9ea376aae445a
zhouhanseng <z308114274@gmail.com>
2023-03-28 12:32:47 +0800

    fix: cmc && crypto rank decimal

--- scripts/providers/cryptoRank.ts
+++ scripts/providers/cryptoRank.ts
@@ -5,6 +5,12 @@ import { differenceBy, some, sortBy, uniqBy } from 'lodash'
 import { toChecksumAddress, isAddress } from 'web3-utils'
 import { cryptoRankcacheDir, delay } from '../utils'
 import getConfig from '../config'
+import { explorerDecimalPageMapping, explorerFetchTokenDecimalMapping } from '../utils/base'
+import puppeteer from 'puppeteer-extra'
+import { executablePath } from 'puppeteer'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+
+puppeteer.use(StealthPlugin())
 
 // data source: https://api.cryptorank.io/v0/tokens/token-platforms
 const PlatformMapping = [
@@ -107,6 +113,9 @@ export class CryptoRank implements Provider {
         }
       })
 
+    const fetchTokenDecimalPage = explorerDecimalPageMapping[chainId]!
+    const fetchTokenDecimal = explorerFetchTokenDecimalMapping[chainId]!
+    const browser = await puppeteer.launch({ executablePath: executablePath(), timeout: 1000000 })
     const topList = uniqBy(sortBy(list, 'rank'), 'address').slice(0, TOTAL)
     const toAddList = differenceBy(topList, exclude, (x) =>
       some(exclude, (e) => x.address.toLowerCase() === e.address.toLowerCase()),
@@ -115,17 +124,26 @@ export class CryptoRank implements Provider {
     console.log(`The total tokens length: is: ${topList.length}`)
     console.log(`The difference tokens length: is: ${toAddList.length}`)
 
-    const result: FungibleToken[] = []
-    for (const token of toAddList) {
-      if (token.symbol === 'eth') continue
+    const allSettled = await Promise.allSettled(
+      toAddList.map(async (x) => {
+        const url = fetchTokenDecimalPage(x.address)
+        try {
+          const decimals = await fetchTokenDecimal(url, browser)
+          if (decimals && decimals > 0) return { ...x, decimals } as FungibleToken
+          return undefined
+        } catch {
+          return undefined
+        }
+      }),
+    )
 
-      result.push({
-        ...token,
-        decimals: 18,
-      })
-      await delay(CR_WAIT_TIME)
-    }
-    return [...result, ...exclude].filter(
+    await browser.close()
+
+    const results = allSettled
+      .map((x) => (x.status === 'fulfilled' && x.value ? x.value : undefined))
+      .filter((x) => Boolean(x)) as FungibleToken[]
+
+    return [...results, ...exclude].filter(
       (x) => x.address && list.find((e) => e.symbol.toLowerCase() === x.symbol.toLowerCase()),
     )
   }
