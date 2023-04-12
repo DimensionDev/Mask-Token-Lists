96493909e586dd29fcf9cbeb3507c96df4e96d65
zhouhanseng <z308114274@gmail.com>
2023-04-12 13:48:06 +0800

    chore: read symbol

--- scripts/commands/readContract.ts
+++ scripts/commands/readContract.ts
@@ -1,3 +1,22 @@
+import { EthereumAddress } from 'wallet.ts'
+import { BaseContract, ERC20 } from '../contract/type'
+import ERC20ABI from '../contract/ERC20.json'
+import type { AbiItem } from 'web3-utils'
+import Web3 from 'web3'
+
 export async function readContract() {
-  console.log(2123)
+  const web3 = new Web3('https://polygon-mainnet.infura.io/v3/d74bd8586b9e44449cef131d39ceeefb')
+  const contract = createContract<ERC20>(web3, '0x750e4C4984a9e0f12978eA6742Bc1c5D248f40ed', ERC20ABI as AbiItem[])
+  console.log(contract?.methods.symbol())
+}
+
+function createContract<T extends BaseContract>(web3: Web3 | null, address: string, ABI: AbiItem[]) {
+  if (!address || !isValidAddress(address) || !web3) return null
+  const contract = new web3.eth.Contract(ABI, address) as unknown as T
+  return contract
+}
+
+function isValidAddress(address?: string): address is string {
+  if (!address) return false
+  return EthereumAddress.isValid(address)
 }
