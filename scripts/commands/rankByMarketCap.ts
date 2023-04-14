e26009e73b1db9a4ea525467b515fae8ab0fe696
zhouhanseng <z308114274@gmail.com>
2023-04-14 17:42:20 +0800

    chore: sort

--- scripts/commands/rankByMarketCap.ts
+++ scripts/commands/rankByMarketCap.ts
@@ -12,11 +12,18 @@ export async function rankByMarketCap(chainId: ChainId, toAddList?: FungibleToke
 
   const tokenListWithRank = await fetch(fetchPage)
 
-  const results = tokenList.map((x) => {
-    const tokenWithRank = tokenListWithRank.find((y) => isSameAddress(x.address, y.address))
-    return tokenWithRank?.rank ? { ...x, rank: tokenWithRank.rank } : x
-  })
+  const results = tokenList
+    .map((x) => {
+      const tokenWithRank = tokenListWithRank.find((y) => isSameAddress(x.address, y.address))
+      return tokenWithRank?.rank ? { ...x, rank: tokenWithRank.rank } : x
+    })
+    .sort((a, z) => {
+      if (z.rank && (!a.rank || a.rank - z.rank > 0)) return 1
+      if (a.rank && (!z.rank || z.rank - a.rank > 0)) return -1
+      return 0
+    })
 
+  console.log({ results })
   if (results.length && !toAddList) {
     await writeTokensToFile(chainId, results)
   }
